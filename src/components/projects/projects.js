03a9cf9270b4d410d026051ccbdc02255e13900c
Christopher Bruner <chrbruner9@gmail.com>
2017-01-23 21:51:16 -0800

    added basic skeleton for all desired states the web app will navigate to for this project

---
+++ src/components/projects/projects.js
@@ -0,0 +1,11 @@
+import template from './projects.html';
+import styles from './projects.scss';
+
+export default {
+    template,
+    controller
+};
+
+function controller() {
+    this.styles = styles;
+};
---
+++ src/components/repos/repos.js
@@ -0,0 +1,11 @@
+import template from './repos.html';
+import styles from './repos.scss';
+
+export default {
+    template,
+    controller
+};
+
+function controller() {
+    this.styles = styles;
+};
